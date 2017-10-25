6c0b23ed2094a02dcb0d29da5ae5ba2d1612f934
Victor Ramos <vramos@windows-8.home>
2017-10-25 02:01:44 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# react-native-expo-geofence
+
+Geofence library for react native, compatible with expo.
+
+Works on iOS and Android.
+
+## Getting started
+
+`$ npm install react-native-expo-geofence --save`
+
+## Expo example
+
+[Maps Example](https://github.com/vramosx/expo-geofence)
+
+## Usage
+```javascript
+import Geofence from 'react-native-expo-geofence';
+
+var points = [
+    { latitude: -23.658739, longitude: -46.666305 },
+    { latitude: -23.651814, longitude:  -46.664129 }
+]
+
+var startPoint = { 
+    latitude: -23.652508,
+    longitude: -46.661474
+}
+
+class MyComponent extends Component
+{
+    getByProximity()
+    {
+        var maxDistanceInKM = 0.5; // 500m distance
+        // startPoint - center of perimeter
+        // points - array of points
+        // maxDistanceInKM - max point distance from startPoint in KM's
+        Geofence.filterByProximity(startPoint, points, maxDistanceInKM);
+    }
+}
+```
+
+
+
---
+++ index.js
@@ -0,0 +1,50 @@
+//import liraries
+import React, { Component } from 'react';
+
+// create a component
+class Geofence extends Component {
+
+    static Log = false;
+
+    static distanceInKM(point1, point2) {
+        var lat1 = point1.latitude;
+        var lon1 = point1.longitude;
+        var lat2 = point2.latitude;
+        var lon2 = point2.longitude;
+
+        var p = 0.017453292519943295;
+        var c = Math.cos;
+        var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+                c(lat1 * p) * c(lat2 * p) * 
+                (1 - c((lon2 - lon1) * p))/2;
+      
+        return 12742 * Math.asin(Math.sqrt(a));
+    }
+    
+    static filterByProximity(centerPoint, points, maxDistance)
+    {
+        var result = [];
+        var pointDistance = 0;
+        var rlog = null;
+        for (var i = 0; i < points.length; i++) {
+            pointDistance = Geofence.distanceInKM(centerPoint, points[i]);
+            if(Geofence.Log)
+            {
+                rlog = "Center Point: { lat:" + centerPoint.latitude + " long: " + 
+                        centerPoint.longitude + " }, Point: { lat: " + points[i].latitude + " long: " +
+                        points[i].longitude + " } - Distance: " +  pointDistance + " KM";
+                console.log(rlog);
+            }
+            
+            if(pointDistance <= maxDistance)
+            {
+                result.push(points[i]);
+            }
+        }
+
+        return result;
+    }
+}
+
+//make this component available to the app
+export default Geofence;
